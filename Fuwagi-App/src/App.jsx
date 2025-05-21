481e9e5736217e3c121c64f753193f3c446eef07
WILLIAM-RUSMANA <93782439+WILLIAM-RUSMANA@users.noreply.github.com>
2025-05-21 22:46:28 +0700

    main

---
+++ Fuwagi-App/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Home from "./pages/Home.jsx"
+import QuizApp from "./pages/Quiz-App.jsx"
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+
+function App() {
+  // const router = createBrowserRouter([
+  //   { path: "/", element: <Home /> },
+  //   { path: "/quiz", element: <QuizApp /> },
+  // ]);
+
+  // return <RouterProvider router={router} />;
+  // return ( <Home />)
+  return ( <QuizApp />);
+}
+
+export default App;
